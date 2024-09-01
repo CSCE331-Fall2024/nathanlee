2179c04e9af731adaa1929dfdfc3664b2ad633a6
NathanL7 <nathan7@tamu.edu>
2024-09-01 12:44:44 -0500

    "Final" draft for career fair, might update first style to look better but I'm proud of my product

--- script.js
+++ script.js
@@ -3,43 +3,40 @@ let flip = document.querySelector("#flip-button");
 let theme = document.querySelector("link#theme");
 let x = false; // Keeps track of the current stylesheet
 
+
 flip.addEventListener("click", () => {
     let i = Math.floor(Math.random() * 2);
     coin.style.animation = "none";
 
     if(i) {
-
         setTimeout(function(){
             coin.style.animation = "headspin 3s forwards";
         }, 100);
 
         // Switch to style2.css if heads
         setTimeout(function(){
-        if (!x) {
-            theme.href = "style2.css";
-            x = true;
-        }
+            if (!x) {
+                theme.href = "style2.css";
+                localStorage.setItem("theme", "style2.css"); // Save the theme in localStorage
+                x = true;
+            }
         }, 3000);
-    } 
-    
-    else {
-
+    } else {
         setTimeout(function(){
             coin.style.animation = "tailspin 3s forwards";
         }, 100);
-        
 
         // Switch to style.css if tails
         setTimeout(function(){
-        if (x) {
-            theme.href = "style.css";
-            x = false;
-        }
+            if (x) {
+                theme.href = "style.css";
+                localStorage.setItem("theme", "style.css"); // Save the theme in localStorage
+                x = false;
+            }
         }, 3000);
-
     }
-    
-    //disables the button as it spins
+
+    // Disables the button as it spins
     disable();
 });
 
